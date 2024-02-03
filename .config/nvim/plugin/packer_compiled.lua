110104f2b5cd032a7a8894714d739cc145e6b6d3
alex <alex.liswandy@hotmail.com>
2024-02-04 05:19:34 +0800

    Added nvim plugin

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -104,6 +104,11 @@ _G.packer_plugins = {
     path = "/Users/ai/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["markdown-preview.nvim"] = {
+    loaded = true,
+    path = "/Users/ai/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
+    url = "https://github.com/iamcco/markdown-preview.nvim"
+  },
   nvim = {
     loaded = true,
     path = "/Users/ai/.local/share/nvim/site/pack/packer/start/nvim",
